ff9aebb802bd5f12d2821eb717ebb2c6c1fa0bc7
BoldFei <1719937412@qq.com>
2018-03-29 22:19:48 +0800

    添加了一些注释

--- 实验一/实验一.cpp
+++ 实验一/实验一.cpp
@@ -13,13 +13,24 @@
 #include <cstring>
 
 using namespace std;
+
+/*待处理串最大长度*/
 const int maxn = 1e6 + 5;
 
 /**
-* 预处理，删除多余的空格，换行，回车，制表符注释
-* 字符串中，忽略空格，注释的影响
-* 注释中，忽略字符串，空格，另一种注释的影响
-* 空格状态能被字符串和注释打断被打断
+* 预处理，删除多余的空格，换行，回车，制表符，注释
+*
+* spaceGot：已经遇到过一个“空格类字符”，接下来的所有“空格类”字符都会被忽略
+* lineCommentGot：已经遇到了“//”，接下来的字符都会被忽略，直到遇到回车字符
+* blockCommentGot：已经遇到了“/*”，接下来的字符都会被忽略，直到遇到“*#(去掉#)/”
+* stringGot：应遇到了“"”，接下来的字符都会被忽略，直到遇到下一个“"”
+*
+* 当前处理字符位于
+* 1.字符串中时，忽略空格，注释的影响
+* 2.注释中时，忽略字符串，空格，另一种注释的影响
+*
+* input: 待处理的串，串最大长度由全局变量maxn指定
+* output: 结果串存放位置，串最大长度由全局变量maxn指定
 */
 void preprocess(char *input, char *output) {
 	int len = strlen(input);
@@ -102,6 +113,53 @@ void preprocess(char *input, char *output) {
 	output[j] = 0;
 }
 
+/*
+关键字，标识符，整数，实数编码
+#include    0
+main        1
+if          2
+then        3
+while       4
+do          5
+static      6
+int         7
+double      8
+struct      9
+break      10
+else       11
+long       12
+switch     13
+case       14
+typedef    15
+char       16
+return     17
+const      18
+float      19
+short      20
+continue   21
+for        22
+void       23
+sizeof     24
+default    25
+ID         27
+integer    28
+real       29
++          30
+-          31
+*          32
+/          33
+:          34
+==         35
+<          36
+!=         37
+<=         38
+>          39
+>=         40
+=          41
+;          42
+(          43
+)          44
+*/
 
 int main() {
 	freopen("data.in", "r", stdin);
